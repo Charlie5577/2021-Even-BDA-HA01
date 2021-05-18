4443227c2a8fe47508f0bbc0142d9007cd8efdb1
Charlie5577 <63338308+Charlie5577@users.noreply.github.com>
2021-05-18 11:17:57 +0530

    Create reducer.py

---
+++ reducer.py
@@ -0,0 +1,12 @@
+from operator import itemgetter
+import sys
+
+sumprob = 0
+
+for line in sys.stdin:
+	res = line.strip('[]')
+	res = res.strip(']\t\n')
+	res = res.split(', ')
+	for r in res:
+		sumprob += float(r)
+	print(sumprob/3)
