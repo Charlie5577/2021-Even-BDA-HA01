9fd5320360c18caadd288dd391746abd01639624
Charlie5577 <63338308+Charlie5577@users.noreply.github.com>
2021-06-10 09:58:21 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -5,26 +5,28 @@ choice_list = [1,2,3]
 prob = []
 
 choice_list = [x-1 for x in choice_list]
-Winning_track=[]
+count = 0
 
 try:
     for li in sys.stdin:
         prob = []
-        for j a range(int(li)):
+        for j in range(int(li)):
             for i in range(1,10000):
                 random.shuffle(prize_list)             
                 my_choice=random.choice(choice_list)      
                 ml= prize_list.copy()    
-                del ml[my_choice]                      
+                #del ml[my_choice]                      
                 if ml[0]=="Empty":
                     del ml[0]
                 else:
                     del ml[1]        
                 if 'Prize'== ml[0]:                     
-                    Winning_track.append(1)    
+                    #Winning_track.append(1)
+                    count+=1
                 else:
-                    Winning_track.append(0)    
-            prob.append(sum(Winning_track)/len(Winning_track))
+                    #Winning_track.append(0) 
+                    count+=0
+            prob=(sum(count)/len(choice_list))
 
         print(prob)
 except e:
